6263edc020a59a55e1b9be85a41ca1dee88038b7
rapunschel <rapunschel.gaming@gmail.com>
2024-10-16 13:12:57 +0200

    Simple navigation bar, with some state to show switched tab

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,50 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return const MaterialApp(home: Center(child: Text('Hello World!')));
+  }
+}
+
+class MainNavigationBar extends StatefulWidget {
+  const MainNavigationBar({super.key});
+
+  @override
+  State<MainNavigationBar> createState() => _MainNavigationBarState();
+}
+
+class _MainNavigationBarState extends State<MainNavigationBar> {
+  int currentTabIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      onDestinationSelected: (int index) {
+        setState(() {
+          currentTabIndex = index;
+        });
+      },
+
+      // indicatorColor: Colors.amber,
+      selectedIndex: currentTabIndex,
+
+      destinations: const <Widget>[
+        NavigationDestination(
+          icon: Icon(Icons.location_on),
+          label: "Home",
         ),
-      ),
+        NavigationDestination(
+          icon: Icon(Icons.search),
+          label: "Search",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.add_circle),
+          label: "Add",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.account_circle),
+          label: "Profile",
+        ),
+      ],
     );
   }
 }
