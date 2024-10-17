795c6438d902feec963ad08e74149aac2fd1daf6
rapunschel <rapunschel.gaming@gmail.com>
2024-10-18 00:08:28 +0200

    Added textformfields for material, instructions

--- lib/screens/add_blue_print_screen.dart
+++ lib/screens/add_blue_print_screen.dart
@@ -1,59 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:nestify/models/blueprint_post.dart';
+import 'package:nestify/widgets/custom_text_form_field.dart';
 
-class AddBluePrintScreen extends StatelessWidget {
-  const AddBluePrintScreen({
+class AddBlueprintScreen extends StatelessWidget {
+  const AddBlueprintScreen({
     super.key,
   });
 
   @override
   Widget build(BuildContext context) {
-    String test = "Hello World";
-    return SafeArea(
-      child: Scaffold(
-        body: Column(
-          children: [
-            Center(
-              child: Text('text: $test',
-                  style: Theme.of(context).textTheme.titleLarge),
-            ),
-            const BluePrintForm()
-          ],
-        ),
-      ),
+    return const SafeArea(
+      child: Scaffold(body: BlueprintForm(isEdit: false)),
     );
   }
 }
 
-class BluePrintForm extends StatefulWidget {
-  const BluePrintForm({super.key});
+class BlueprintForm extends StatefulWidget {
+  const BlueprintForm({super.key, required this.isEdit});
+
+  final bool isEdit;
 
   @override
-  BluePrintFormState createState() {
-    return BluePrintFormState();
+  BlueprintFormState createState() {
+    return BlueprintFormState();
   }
 }
 
 // Define a corresponding State class.
 // This class holds data related to the form.
-class BluePrintFormState extends State<BluePrintForm> {
+class BlueprintFormState extends State<BlueprintForm> {
   // Create a global key that uniquely identifies the Form widget
   // and allows validation of the form.
   //
   // Note: This is a `GlobalKey<FormState>`,
   final _formKey = GlobalKey<FormState>();
+  final BlueprintPost blueprint = BlueprintPost();
 
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
     return Padding(
-      padding: const EdgeInsets.only(left: 32, right: 32, bottom: 16),
+      padding: const EdgeInsets.only(left: 32, right: 32, bottom: 16, top: 16),
       child: Form(
         key: _formKey,
         child: Column(
           children: <Widget>[
+            Center(
+              child: Text("Upload blueprint post",
+                  style: Theme.of(context).textTheme.titleLarge),
+            ),
             // Add TextFormFields and ElevatedButton here.
-            CustomTextFormField(),
-
+            titleFormField(),
+            materialFormField(),
+            instructionFormField(),
             ElevatedButton(
               // TODO change in the future
               onPressed: () {
@@ -62,7 +61,9 @@ class BluePrintFormState extends State<BluePrintForm> {
                   // If the form is valid, display a snackbar. In the real world,
                   // you'd often call a server or save the information in a database.
                   ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Processing Data')),
+                    const SnackBar(
+                      content: Text("Processing"),
+                    ),
                   );
                 }
               },
@@ -73,46 +74,38 @@ class BluePrintFormState extends State<BluePrintForm> {
       ),
     );
   }
-}
 
-class CustomTextFormField extends StatefulWidget {
-  const CustomTextFormField(
-      {super.key, this.onChanged, this.initialValue, this.inputDecoration});
-
-  final Function(String)? onChanged;
-  final String? initialValue;
-  final InputDecoration? inputDecoration;
-
-  @override
-  State<CustomTextFormField> createState() => _CustomTextFormFieldState();
-}
-
-class _CustomTextFormFieldState extends State<CustomTextFormField> {
-  final TextEditingController textController = TextEditingController();
-
-  // Dispose to prevent memory leaks
-  @override
-  void dispose() {
-    textController.dispose();
-    super.dispose();
+  CustomTextFormField titleFormField() {
+    return CustomTextFormField(
+        initialValue: widget.isEdit ? blueprint.title : null,
+        onChanged: (value) {
+          blueprint.title = value;
+        },
+        maxLength: 60,
+        label: const Text("Title"),
+        hintText: "Title of your creation",
+        errorText: "Please enter a title");
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return TextFormField(
-      initialValue: widget.initialValue,
-      // controller: textController,
-      decoration: widget.inputDecoration,
-      onChanged: (value) {
-        debugPrint("Text is: ${value}");
-      },
-      // The validator receives the text that the user has entered.
-      validator: (value) {
-        if (value == null || value.isEmpty) {
-          return 'Please enter some text';
-        }
-        return null;
-      },
-    );
+  CustomTextFormField materialFormField() {
+    return CustomTextFormField(
+        initialValue: widget.isEdit ? blueprint.material : null,
+        onChanged: (value) {
+          blueprint.material = value;
+        },
+        label: const Text("Material"),
+        hintText: "Recommended material",
+        errorText: "Please enter materials");
+  }
+
+  CustomTextFormField instructionFormField() {
+    return CustomTextFormField(
+        initialValue: widget.isEdit ? blueprint.instruction : null,
+        onChanged: (value) {
+          blueprint.instruction = value;
+        },
+        label: const Text("Instructions"),
+        hintText: "Describe how to build your creation",
+        errorText: "Please enter instructions");
   }
 }
