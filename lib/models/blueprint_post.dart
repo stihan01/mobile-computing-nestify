4c2465107e67e9bd243f90e9da8c45b1e16f8c23
rapunschel <rapunschel.gaming@gmail.com>
2024-10-17 22:49:26 +0200

    Set default value to parameters

--- lib/models/blueprint_post.dart
+++ lib/models/blueprint_post.dart
@@ -1,9 +1,9 @@
 class BlueprintPost {
   late String _id;
-  String? _title;
-  String? _description;
+  String _title;
+  String _description;
 
-  BlueprintPost([this._title, this._description]) {
+  BlueprintPost([this._title = "", this._description = ""]) {
     _id = DateTime.now().millisecondsSinceEpoch.toString();
   }
 
@@ -18,16 +18,16 @@ class BlueprintPost {
         "description": _description,
       };
 
-  String? get title => _title;
+  String get title => _title;
   String get id => _id;
-  String? get description => _description;
+  String get description => _description;
 
   // TODO, update value in DB.
-  set title(String? newValue) {
+  set title(String newValue) {
     _title = newValue;
   }
 
-  set description(String? newValue) {
+  set description(String newValue) {
     _description = newValue;
   }
 }
