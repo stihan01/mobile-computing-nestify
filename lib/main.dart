b2fdcb1b21b0bb1b67f79f17b4f48954692afde9
Adenholm <hanni-01@live.se>
2024-10-21 10:20:56 +0200

    added shellrouter

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 import '/utils/router.dart';
 
 void main() {
   runApp(const MainApp());
 }
-
-//Test line
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -14,7 +11,75 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      routerConfig: router,
+      debugShowCheckedModeBanner: false,
+      routerConfig: router,);
+  }
+}
+
+class ScaffoldWithNestedNavigation extends StatelessWidget {
+  const ScaffoldWithNestedNavigation({
+    Key? key,
+    required this.navigationShell,
+  }) : super(key: key ?? const ValueKey('ScaffoldWithNestedNavigation'));
+  final StatefulNavigationShell navigationShell;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: navigationShell,
+      bottomNavigationBar: NavigationBar(
+        selectedIndex: navigationShell.currentIndex,
+        destinations: const [
+          NavigationDestination(label: 'Section A', icon: Icon(Icons.home)),
+          NavigationDestination(label: 'Section B', icon: Icon(Icons.settings)),
+        ],
+        onDestinationSelected: _goBranch,
+      ),
+    );
+  }
+}
+
+
+class MainNavigationBar extends StatefulWidget {
+  const MainNavigationBar({super.key});
+
+  @override
+  State<MainNavigationBar> createState() => _MainNavigationBarState();
+}
+
+class _MainNavigationBarState extends State<MainNavigationBar> {
+  int currentTabIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return NavigationBar(
+      onDestinationSelected: (int index) {
+        setState(() {
+          currentTabIndex = index;
+        });
+      },
+
+      // indicatorColor: Colors.amber,
+      selectedIndex: currentTabIndex,
+
+      destinations: const <Widget>[
+        NavigationDestination(
+          icon: Icon(Icons.location_on),
+          label: "Home",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.search),
+          label: "Search",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.add_circle),
+          label: "Add",
+        ),
+        NavigationDestination(
+          icon: Icon(Icons.account_circle),
+          label: "Profile",
+        ),
+      ],
     );
   }
 }
