c33a88214068fc37da1b7e76f515c430c7a97c45
rapunschel <rapunschel.gaming@gmail.com>
2024-10-21 18:11:22 +0200

    added field for category

--- lib/models/blueprint_post.dart
+++ lib/models/blueprint_post.dart
@@ -2,27 +2,29 @@ import 'package:image_picker/image_picker.dart';
 
 class BlueprintPost {
   late String _id;
-  late String title;
-  late String material;
-  late String instruction;
-
+  late String? title;
+  late String? material;
+  late String? instruction;
+  late String? category;
   List<XFile> images = [];
 
-  BlueprintPost([this.title = "", this.material = "", this.instruction = ""]) {
+  BlueprintPost([this.title, this.material, this.instruction, this.category]) {
     _id = DateTime.now().millisecondsSinceEpoch.toString();
   }
 
   BlueprintPost.fromJson(Map<String, dynamic> json)
-      : _id = json['id'] as String,
+      : _id = json['post_id'] as String,
         title = json['title'],
         material = json['material'],
-        instruction = json['instruction'];
+        instruction = json['instruction'],
+        category = json['category'];
 
   Map<String, dynamic> toJson() => {
-        "id": _id,
+        "post_id": _id,
         "title": title,
         "material": material,
-        "instruction": instruction
+        "instruction": instruction,
+        'category': category,
       };
 
   get id => _id;
