cea1d876c62cfc15a331df74d0cfd973ee6c706c
stihan01 <stihan01@gmail.com>
2024-10-23 12:06:14 +0200

    added search bar

--- lib/pages/searchPage.dart
+++ lib/pages/searchPage.dart
@@ -1,19 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
 
+  @override
+  State<SearchPage> createState() => _SearchpageState();
+}
+
+class _SearchpageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Search Screen')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => context.go('/'),
-          child: const Text('Go back to the Home screen'),
-        ),
-      ),
+      body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 16),
+          child: Center(
+              child: Column(
+            children: [
+              SearchAnchor(
+                  builder: (BuildContext context, SearchController controller) {
+                return SearchBar(
+                  elevation: WidgetStatePropertyAll(2),
+                  controller: controller,
+                  padding: const WidgetStatePropertyAll<EdgeInsets>(
+                      EdgeInsets.symmetric(horizontal: 16.0)),
+                  onTap: () {
+                    controller.openView();
+                  },
+                  onChanged: (_) {
+                    controller.openView();
+                  },
+                  leading: const Icon(Icons.search),
+                );
+              }, suggestionsBuilder:
+                      (BuildContext context, SearchController controller) {
+                return List<ListTile>.generate(5, (int index) {
+                  final String item = 'item $index';
+                  return ListTile(
+                    title: Text(item),
+                    onTap: () {
+                      setState(() {
+                        controller.closeView(item);
+                      });
+                    },
+                  );
+                });
+              }),
+              ElevatedButton(
+                onPressed: () => context.go('/'),
+                child: const Text('Go back to the Home screen'),
+              ),
+            ],
+          ))),
     );
   }
 }
