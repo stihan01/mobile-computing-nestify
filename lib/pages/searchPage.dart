f35e80bb925f8ab8f8323417b1f67a6b3279fdfb
stihan01 <stihan01@gmail.com>
2024-10-24 20:11:19 +0200

    added filter and  search button

--- lib/pages/searchPage.dart
+++ lib/pages/searchPage.dart
@@ -18,41 +18,74 @@ class _SearchpageState extends State<SearchPage> {
           child: Center(
               child: Column(
             children: [
-              SearchAnchor(
-                  builder: (BuildContext context, SearchController controller) {
-                return SearchBar(
-                  elevation: WidgetStatePropertyAll(2),
-                  controller: controller,
-                  padding: const WidgetStatePropertyAll<EdgeInsets>(
-                      EdgeInsets.symmetric(horizontal: 16.0)),
-                  onTap: () {
-                    controller.openView();
-                  },
-                  onChanged: (_) {
-                    controller.openView();
-                  },
-                  leading: const Icon(Icons.search),
-                );
-              }, suggestionsBuilder:
-                      (BuildContext context, SearchController controller) {
-                return List<ListTile>.generate(5, (int index) {
-                  final String item = 'item $index';
-                  return ListTile(
-                    title: Text(item),
-                    onTap: () {
-                      setState(() {
-                        controller.closeView(item);
-                      });
-                    },
-                  );
-                });
-              }),
+              _SearchBar(),
+              Padding(
+                padding: const EdgeInsets.all(16),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    OutlinedButton.icon(
+                      onPressed: show,
+                      icon: const Icon(Icons.filter_alt),
+                      label: const Text('Filter'),
+                    ),
+                    OutlinedButton.icon(
+                      icon: const Icon(Icons.sort),
+                      label: const Text('Sort'),
+                      onPressed: () {},
+                    ),
+                  ],
+                ),
+              ),
               ElevatedButton(
                 onPressed: () => context.go('/'),
                 child: const Text('Go back to the Home screen'),
               ),
             ],
           ))),
+    );
+  }
+
+  Widget _SearchBar() {
+    return SearchAnchor(
+        builder: (BuildContext context, SearchController controller) {
+      return SearchBar(
+        elevation: WidgetStatePropertyAll(2),
+        controller: controller,
+        padding: const WidgetStatePropertyAll<EdgeInsets>(
+            EdgeInsets.symmetric(horizontal: 16.0)),
+        onTap: () {
+          controller.openView();
+        },
+        onChanged: (_) {
+          controller.openView();
+        },
+        leading: const Icon(Icons.search),
+      );
+    }, suggestionsBuilder: (BuildContext context, SearchController controller) {
+      return List<ListTile>.generate(5, (int index) {
+        final String item = 'item $index';
+        return ListTile(
+          title: Text(item),
+          onTap: () {
+            setState(() {
+              controller.closeView(item);
+            });
+          },
+        );
+      });
+    });
+  }
+
+  void show() {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return Container(
+          height: 200,
+          child: Center(child: Text("this is a filter modal")),
+        );
+      },
     );
   }
 }
